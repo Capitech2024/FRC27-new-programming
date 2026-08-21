f3ee6331440d55a9d926e6ee2176db8e92a7edb0
Capitech10356-1 <jorgehenriquealda23@gmail.com>
2026-08-21 17:46:17 -0400

    Add expansive intake in TeleOp, add cases of current state to switch in StateMachine, changed the name of the object currenstate to currentState, changed the PS4 controllers to Xbox controllers, changed the buttons to Xbox base buttons and create the new object Retract for the timer of INTAKE_RETRAC

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot; //Importação para o robô executar seu código em 20ms
@@ -79,6 +75,9 @@ public class Robot extends TimedRobot {
   private boolean lastR1Button = false;
   private boolean lastL1Button = false;
   private boolean lastTriangleButton = false;
+  private boolean lastCircleButton = false;
+  private boolean lastSquareButton = false;
+  private boolean last
 
   private final SparkMaxConfig driveConfig = new SparkMaxConfig();
   private final PS4Controller controller1 = new PS4Controller(0);
